eb01e61f4136e3abcfffa8325b4dd943bc622afc
peterstechblog <12556909+peterstechblog@users.noreply.github.com>
2018-11-07 18:41:13 +0000

    add return code

--- benchmark_io/start.sh
+++ benchmark_io/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-printf "env vars\n"
+printf "### env vars ###\n"
 printf "work_dir=$work_dir\n"
 printf "file_size=$file_size\n"
 printf "time=$time\n"
 printf "delay=$delay\n"
 
 /benchmark_io --work-dir=$work_dir --file-size=$file_size --time=$time --delay=$delay
+status=$?
 cat ${work_dir}/logs/*.log
-exit 0
+exit $status
