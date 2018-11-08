ed055cfda4855906cb03e72e909fb940687cadcd
peterstechblog <12556909+peterstechblog@users.noreply.github.com>
2018-11-08 00:19:50 +0000

    add boto tests

--- benchmark_io/docker/start.sh
+++ benchmark_io/docker/start.sh
@@ -4,6 +4,7 @@ printf "work_dir=$work_dir\n"
 printf "file_size=$file_size\n"
 printf "time=$time\n"
 printf "delay=$delay\n"
+printf "################\n\n"
 
 /benchmark_io --work-dir=$work_dir --file-size=$file_size --time=$time --delay=$delay
 status=$?
